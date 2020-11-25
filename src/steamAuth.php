6e70e1bd5320f8e59621bc2068dae2643588f957
João Pedro <52453013+devpremium@users.noreply.github.com>
2020-11-24 21:06:09 -0300

    Att

--- src/steamAuth.php
+++ src/steamAuth.php
@@ -24,26 +24,9 @@ class steamAuth extends LightOpenID
 					$url = file_get_contents("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=".
 						$steamauth['apikey'].
 						"&steamids=".$matches[1]
-					);
-
-					$content = json_decode($url, true);
-
-					$steam_data = (object)[
-						'steam_steamid'                  => $content['response']['players'][0]['steamid'],
-						'steam_communityvisibilitystate' => $content['response']['players'][0]['communityvisibilitystate'],
-						'steam_profilestate'             => 'nd',
-						'steam_personaname'              => $content['response']['players'][0]['personaname'], 
-						'steam_profileurl'               => $content['response']['players'][0]['profileurl'], 
-						'steam_avatar'                   => $content['response']['players'][0]['avatar'], 
-						'steam_avatarmedium'             => $content['response']['players'][0]['avatarmedium'], 
-						'steam_avatarfull'               => $content['response']['players'][0]['avatarfull'], 
-						'steam_personastate'             => $content['response']['players'][0]['personastate'], 
-						'steam_realname'                 => isset($content['response']['players'][0]['realname']) ? :'Real name not given', 
-						'steam_timecreated'              => $content['response']['players'][0]['timecreated'], 
-						'steam_uptodate'                 => time()
-                    ];
+                    );
                     
-                    return $steam_data;
+                    return json_decode($url)->response->players[0];
                 }else{
                     return 'OpenID Error';
                 }
--- test.php
+++ test.php
@@ -1,18 +1,26 @@
 <?php
-
+define("url", "http://localhost:3330");
 /**
  * Login
  */
+
 use steamLogin\steamAuth;
 
 class login
 {
     public static function steam()
     {
-        require_once __DIR__.'/vendor/autoload.php';
+        require_once __DIR__ . '/vendor/autoload.php';
 
-        echo steamAuth::login([]);
+        $loginSteam = steamAuth::login([
+            'apikey'     => '2C2B1FFEF87C06A430319DFFBB52DB38',
+            'domainname' => url,
+            'logoutpage' => url . '/logout',
+            'loginpage'  => url . '/test.php'
+        ]);
+        
+        print_r($loginSteam);
     }
 }
 
-echo login::steam();
+login::steam();
